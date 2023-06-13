17e9bd257f24b5c95c214ab3757a4f8552bf30b7
Luiz Vieira <ext_luizjuni@mercadolibre.com>
2023-06-13 11:56:47 -0300

    changes

--- src/main.py
+++ src/main.py
@@ -4,23 +4,6 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 VERIFY_TOKEN = 'mvp-bot'
 
-def process_webhook_notification(self, data):
-        """_summary_: Process webhook notification
-        For the moment, this will return the type of notification
-        """
-        response = []
-
-        for entry in data["entry"]:
-            for change in entry["changes"]:
-                response.append(
-                    {
-                        "type": change["field"],
-                        "from": change["value"]["metadata"]["display_phone_number"],
-                    }
-                )
-        # Do whatever with the response
-        return response
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -28,16 +11,25 @@ def hello_world():
 @app.route("/webhook/", methods=["POST", "GET"])
 def webhook_whatsapp():
     """__summary__: Get message from the webhook"""
-    print(request.method)
     if request.method == "GET":
         if request.args.get('hub.verify_token') == VERIFY_TOKEN:
             return request.args.get('hub.challenge')
         return "Authentication failed. Invalid Token."
 
-    print (request.get_json())
-    print (request)
-    # response = process_webhook_notification(request.get_json())
-    # print(response) 
+
+    response = request.get_json()
+    data = {}
+    for entry in response["entry"]:
+        for change in entry["changes"]:
+            data['type'] = change["field"]
+            data['number'] = change["value"]["metadata"]["display_phone_number"]
+            data['name'] = change["value"]['contacts']['profile']['name']
+            data['msg'] = change["value"]['messages']['text']['body']
+
+    if data['msg'] not in ['1','2']:
+        print(f'''Olá{data['name']}, seja bem vindo ao bookBot \n
+                digite 1 para teste 1, digite 2 para teste 2 :)''')
+                 
     # Do anything with the response
     # Sending a message to a phone number to confirm the webhook is working
 
